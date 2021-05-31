5adf4ef01a122dad70effb135bb48391db7f8e60
Onejledonlala <78067427+Onejledonlala@users.noreply.github.com>
2021-05-30 22:31:50 -0400

    Deliverable 2 completed

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -7,3 +7,11 @@ MechaCar_lm <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground
 
 summary(MechaCar_lm)
 
+Suspension_Coil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+Suspension_Coil
+
+total_summary <- Suspension_Coil %>% summarize(Mean =mean(PSI), Median =median(PSI), Variance =var(PSI), SD =sd(PSI), .groups = 'keep') #create summary table`
+total_summary
+
+lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean =mean(PSI), Median =median(PSI), Variance =var(PSI), SD =sd(PSI), .groups = 'keep') #create summary table` 
+lot_summary
