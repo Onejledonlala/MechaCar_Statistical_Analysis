bc7fb6a6952eb850d093b74648ba181c93c1eb3f
Onejledonlala <78067427+Onejledonlala@users.noreply.github.com>
2021-05-31 02:07:11 -0400

    module completed

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -15,3 +15,11 @@ total_summary
 
 lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean =mean(PSI), Median =median(PSI), Variance =var(PSI), SD =sd(PSI), .groups = 'keep') #create summary table` 
 lot_summary
+
+t.test(Suspension_Coil$PSI,mu=1500)
+
+t.test(subset(Suspension_Coil,Manufacturing_Lot=="Lot1")$PSI,mu = 1500)
+
+t.test(subset(Suspension_Coil,Manufacturing_Lot=="Lot2")$PSI,mu = 1500)
+
+t.test(subset(Suspension_Coil,Manufacturing_Lot=="Lot3")$PSI,mu = 1500)
